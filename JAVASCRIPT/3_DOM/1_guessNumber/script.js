76a898318d5a9c8e4890c3c7bbe21b38d6b4edb1
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-12-26 20:32:06 -0600

    Guess the number game

---
+++ JAVASCRIPT/3_DOM/1_guessNumber/script.js
@@ -0,0 +1,66 @@
+"use strict";
+
+/*
+console.log(document.querySelector(".message"));
+console.log(document.querySelector(".message").textContent); // Start guessing...
+
+document.querySelector(".message").textContent = "Correct Number!";
+console.log(document.querySelector(".message").textContent); // Correct Number!
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 23;
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  // when there is no input
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number";
+
+    // when player wins
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct number!";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    // when guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too high";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+
+    // when guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
