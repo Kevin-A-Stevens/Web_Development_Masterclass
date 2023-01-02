12b06fd1a9f06a840811ef5e2e6d6f459eeada25
Kevin Stevens <kevin.alan.stevens@gmail.com>
2023-01-01 22:37:12 -0600

    The Pig game complete

---
+++ JAVASCRIPT/3_DOM/2_ModalWindow/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// select classes you need and store in variables
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// add a click event to multiple elements
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+// add a click event to one element
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
