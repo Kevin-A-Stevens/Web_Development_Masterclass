c426c12758aa3ddac8b6f2859f582901b3a3e65a
Kevin Stevens <kevin.alan.stevens@gmail.com>
2023-01-13 21:05:01 -0600

    todo list functionality

--- PROJECTS/3_todolist-v1/app.js
+++ PROJECTS/3_todolist-v1/app.js
@@ -3,40 +3,32 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = ["Buy food", "Cook food", "Eat food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   let today = new Date();
-  let currentDay = today.getDay();
-  let day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: Current day is equal to: " + currentDay);
-  }
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-  res.render("list", { kindOfDay: day });
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
